b9c21fc896659624c2ec249dac63a5f49c7f82ba
Andrew PG <andrewpgibbs71@gmail.com>
2022-10-10 23:31:19 -0400

    basic javascript framework set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,37 @@
-const APIKey = f9385a3d1330702c97d30400e846f50e;
-var city;
+const APIKey = "f9385a3d1330702c97d30400e846f50e";
+var city = "Orlando";
+var lat = "";
+var lon = "";
+function getCoordinatesFromOpenWeatherMap(city) {
+    var geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+
+    fetch(geocodingUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log("city = ", city, "data = ", data)
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        getForecastWeatherFromOpenWeatherMap(city)
+    })
+    .catch( function (error){
+        console.log(error);
+    })
+}
+getCoordinatesFromOpenWeatherMap(city);
+
+function getForecastWeatherFromOpenWeatherMap(city) {
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+
+    fetch(forecastUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log("city = ", city, "data = ", data)
+    })
+    .catch( function (error){
+        console.log(error);
+    })
+}
