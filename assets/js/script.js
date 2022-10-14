f7beab50ab7feb7e81e8210cf476110a020e1556
Andrew PG <andrewpgibbs71@gmail.com>
2022-10-14 17:35:17 -0400

    woring on functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,68 @@
 const APIKey = "f9385a3d1330702c97d30400e846f50e";
-var city = document.getElementById("city").value;
-var lat = "";
-var lon = "";
+var city ="orlando" //document.getElementById("city").value;
+var lat = "28.5383";
+var lon = "-81.3792";
 const searchButton = document.getElementById("searchButton");
 
-// searchButton.addEventListener("click", getCoordinatesFromOpenWeatherMap);
-searchButton.addEventListener("click", () => getCoordinatesFromOpenWeatherMap(city));
+
+// searchButton.addEventListener("click", () => getCoordinatesFromOpenWeatherMap(city));
 
 
-
-
-function getCoordinatesFromOpenWeatherMap(city) {
+function getCoordinatesFromOpenWeatherMap(city){
     var geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-
+    
     fetch(geocodingUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log("city = ", city, "data = ", data)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('city = ', city, 'data =', data)});
         lat = data.coord.lat;
         lon = data.coord.lon;
         getForecastWeatherFromOpenWeatherMap(city)
-    })
-    .catch( function (error){
-        console.log(error);
-    })
-    console.log(city);
+
+        .catch((err) => console.log(err));
+        console.log(city);
+
 }
+
 // getCoordinatesFromOpenWeatherMap(city);
 
-function getForecastWeatherFromOpenWeatherMap(city) {
-    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+// function getForecastWeatherFromOpenWeatherMap(city) {
+//     var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
 
+//     fetch(forecastUrl)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (data){
+//         console.log("city = ", city, "data = ", data)
+//     })
+//     .catch( function (error){
+//         console.log(error);
+//     })
+// }
+
+function getForecastWeatherFromOpenWeatherMap(city){
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
     fetch(forecastUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log("city = ", city, "data = ", data)
-    })
-    .catch( function (error){
-        console.log(error);
-    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log("city =", city, "data =", data)})
+        .catch((err) => console.log(err));
+        console.log(city)
+
+
 }
+
+function handleSearchFormSubmit(e){
+    e.preventDefault();
+    console.log(e.target)
+var rawSearch = city.value.trim();
+var search = rawSearch.charAt(0).toUpperCase() + rawSearch.slice(1).toLowerCase();
+console.log(search);
+getCoordinatesFromOpenWeatherMap(search);
+gameSearch.value = '';
+}
+
+searchButton.addEventListener("click", getCoordinatesFromOpenWeatherMap(city));
+searchButton.addEventListener("click", getForecastWeatherFromOpenWeatherMap(city));
+searchForm.addEventListener('submit', handleSearchFormSubmit);
