8c4f58d2e7b0afa96faea3d25ac32f31cc397299
Andrew PG <andrewpgibbs71@gmail.com>
2023-03-28 20:50:15 -0400

    search results now reflect the city name with proper grammar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 const APIKey = "f9385a3d1330702c97d30400e846f50e";
-var city = "orlando";//document.getElementById("city").value;
+var city = document.getElementById("city-name").value;
 var lat = "";
 var lon = "";
 var largeHeading = document.getElementById('searchResult');
-// const searchButton = document.getElementById("searchButton");
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", getCoordinatesFromOpenWeatherMap);
 
-// searchButton.addEventListener("click", getCoordinatesFromOpenWeatherMap);
-// searchButton.addEventListener("click", () => getCoordinatesFromOpenWeatherMap(city));
 
 function fiveDayForecast(data) {
 
@@ -17,45 +16,43 @@ function fiveDayForecast(data) {
     }
 }
  function sendDataToPage(data) {
-    cityName = data.name;
+    
     farenheightTemp = parseFloat(data.main.temp)
-    largeHeading.textContent = cityName;
+
+    cityName = document.getElementById("city-name").value
+    cityNameUppercase = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+    largeHeading.textContent =cityNameUppercase
+    console.log(largeHeading)
+
+     
     console.log("console log from sendDataToPage function")
     document.getElementById('temp-main').textContent = "Temperature : "+ (Math.round(((farenheightTemp - 273.15) * 9/5 + 32)))+ " degrees Farenheit";
     document.getElementById('hum-main').textContent = "Humidity : "+ data.main.humidity + "%";
     document.getElementById('wind-main').textContent = "Wind speeds : "+ data.wind.speed+ "mph"
  }
- function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputFahrenheit").innerHTML=((valNum-273.15)*1.8)+32;
-  }
-function getCoordinatesFromOpenWeatherMap(city) {
-    var geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
 
+function getCoordinatesFromOpenWeatherMap() {
+    var geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
     fetch(geocodingUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data){
-        // console.log("city = ", city, "data = ", data)
         lat = data.coord.lat;
         lon = data.coord.lon;
         sendDataToPage(data);
-   
         getForecastWeatherFromOpenWeatherMap(data)
     })
     .catch( function (error){
         console.log(error);
     })
-    console.log(city);
-    
+    console.log(city);    
 }
-getCoordinatesFromOpenWeatherMap(city);
+
 
 function getForecastWeatherFromOpenWeatherMap(city) {
     var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
-
-    fetch(forecastUrl)
+   fetch(forecastUrl)
     .then(function (response){
         return response.json();
     })
